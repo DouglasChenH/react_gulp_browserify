3ec38e93bf9440a6d5d4944f0730f3859ab797a4
Douglas <douglas@accurenergy.com>
2018-04-26 11:20:46 -0400

    add browserify to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,25 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var react = require('gulp-react');
-var htmlreplace = require('gulp-html-replace');
 
 var livereload = require('gulp-livereload');
 var webserver = require('gulp-webserver');
 
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var htmlreplace = require('gulp-html-replace');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var streamify = require('gulp-streamify');
+
 var path = {
   HTML: 'src/index.html',
-  ALL: ['src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
-  JS: ['src/js/*.js', 'src/js/**/*.js'],
   MINIFIED_OUT: 'build.min.js',
-  DEST_SRC: 'dist/src',
+  OUT: 'build.js',
+  DEST: 'dist',
   DEST_BUILD: 'dist/build',
-  DEST: 'dist'
+  DEST_SRC: 'dist/src',
+  ENTRY_POINT: './src/js/App.js'
 };
-
-//  transforms JSX into JS
-gulp.task('transform', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(gulp.dest(path.DEST_SRC));
-});
 
 //  take index.html file and copy it over to dist folder 
 //  so newly created JS files from transform Gulp task above
@@ -32,17 +29,33 @@ gulp.task('copy', function(){
     .pipe(gulp.dest(path.DEST));
 });
 
-/*
- 	a task that will always be running 
- 	so when we change either index.html or any of the JS files, 
- 	other tasks from earlier will run and update the code in the dist folder.
-*/
-gulp.task('watch', function(){
-  gulp.watch(path.ALL, ['transform', 'copy']);
+
+
+gulp.task('watch', function() {
+  gulp.watch(path.HTML, ['copy']);  // watch index.html, run copy task if changes
+
+var watcher  = watchify(browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source(path.OUT))
+      .pipe(gulp.dest(path.DEST_SRC))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source(path.OUT))
+    .pipe(gulp.dest(path.DEST_SRC));
 });
 
+
+
 gulp.task('webserver',function () {
-  gulp.src('src')  // 服务器根目录
+  gulp.src(path.DEST)  // 服务器根目录
   .pipe(webserver({   // 运行webserver
     livereload: true,    // 启用livereload
     open: true  // 服务器启动时自动打开网页
@@ -50,20 +63,22 @@ gulp.task('webserver',function () {
 });
 
 
-gulp.task('default', ['webserver','watch']);
+gulp.task('default', ['watch']);
 
 /*
   grab all of JS files, concatenate all of them together, 
   minify them, then output the result to  dist/build folder. 
 */
 gulp.task('build', function(){
-  gulp.src(path.JS)
-    .pipe(react())
-    .pipe(concat(path.MINIFIED_OUT))
-    .pipe(uglify(path.MINIFIED_OUT))
+  browserify({
+    entries: [path.ENTRY_POINT],
+    transform: [reactify]
+  })
+    .bundle()
+    .pipe(source(path.MINIFIED_OUT))
+    .pipe(streamify(uglify(path.MINIFIED_OUT)))
     .pipe(gulp.dest(path.DEST_BUILD));
 });
-
 
 /*
   htmlreplace is an object with a key that represents where to replace 
